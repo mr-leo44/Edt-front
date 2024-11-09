6ad8de3d1a41619f96425b8a915d875227c043a3
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-09 14:01:51 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+import { resolve } from 'path';
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
+  app: {
+    head: {
+      title: 'Wakati App',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width-device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Application de gestion des emploi du temps dans le milieu universitaire' },
+        { name: 'format-detection', content: 'telephone-no' },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        }
+      ],
+    }
+  },
+  css: ['~/assets/css/index.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
+  // modules: ["@pinia/nuxt", "@nuxt/icon"]
+});
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Woubi
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
