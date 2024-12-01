95a4866251d681adb45d3402615eecaa48a67d85
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-01 15:31:26 +0100

    add shacn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,43 @@
-import { resolve } from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
+  modules: [
+    // "@pinia/nuxt",
+    "@nuxt/icon",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+  ],
   app: {
     head: {
-      title: 'Wakati App',
+      title: "Wakati App",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width-device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Application de gestion des emploi du temps dans le milieu universitaire' },
-        { name: 'format-detection', content: 'telephone-no' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Application de gestion des emploi du temps dans le milieu universitaire",
+        },
+        { name: "format-detection", content: "telephone-no" },
       ],
       link: [
         {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/favicon.ico'
-        }
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
       ],
-    }
+    },
   },
-  modules: [// "@pinia/nuxt", 
-  "@nuxt/icon", '@nuxtjs/tailwindcss']
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css'
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classSuffix: '',
+  },
 });
