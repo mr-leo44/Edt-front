bef627b62975489e1aa27fdcdcc45a89f07dc989
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-01 17:55:38 +0100

    add card component

---
+++ components/ui/card/CardDescription.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <p :class="cn('truetext-sm truetext-muted-foreground', props.class)">
+    <slot />
+  </p>
+</template>
---
+++ components/ui/card/CardFooter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <div :class="cn('trueflex trueitems-center truep-6 truept-0', props.class)">
+    <slot />
+  </div>
+</template>
