38dc3bc95c39729052d7cf04d3e36609af26fc73
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-01 16:18:33 +0100

    add label component

---
+++ components/ui/label/Label.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+import { Label, type LabelProps } from 'radix-vue'
+import { computed, type HTMLAttributes } from 'vue'
+
+const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <Label
+    v-bind="delegatedProps"
+    :class="
+      cn(
+        'truetext-sm truefont-medium trueleading-none peer-disabled:truecursor-not-allowed peer-disabled:trueopacity-70',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </Label>
+</template>
