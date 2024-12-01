00a29ad3f1948069c0eef8c6274c796c48c08b2c
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-01 16:16:37 +0100

    add Form component

---
+++ components/ui/form/FormDescription.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { useFormField } from './useFormField'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+
+const { formDescriptionId } = useFormField()
+</script>
+
+<template>
+  <p
+    :id="formDescriptionId"
+    :class="cn('truetext-sm truetext-muted-foreground', props.class)"
+  >
+    <slot />
+  </p>
+</template>
