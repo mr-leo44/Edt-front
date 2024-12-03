222085668676fbf3977a238adbea0f0c42a659f8
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-03 05:42:50 +0100

    update forgot password

--- pages/forgot-password/index.vue
+++ pages/forgot-password/index.vue
@@ -12,7 +12,25 @@
           Veuillez entrer l'adresse email associée à votre compte. un code de vérification vous sera envoyé pour
           confirmer votre identité.
         </p>
-        
+        <form @submit.prevent="submit" class="flex flex-col">
+          <div class="space-y-3 w-full">
+            <FormField v-slot="{ componentField }" name="email">
+              <FormItem>
+                <FormLabel class="text-xs md:text-sm text-neutral-700 font-semibold">Email</FormLabel>
+                <FormControl>
+                  <Input type="email"
+                    class="w-full p-2 sm:px-4 text-sm sm:text-md rounded text-neutral-800 border border-neutral-600 focus:border-neutral-700 outline-none"
+                    v-model="formData.email" autocomplete="off" v-bind="componentField" />
+                </FormControl>
+              </FormItem>
+            </FormField>
+
+            <Button variant="outline"
+              class="p-2.5 bg-emerald-500 rounded text-white hover:bg-emerald-700 hover:text-white font-semibold text-sm 2xl:text-md uppercase border-none">
+              Envoyer
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -21,6 +39,9 @@
 <script lang="ts" setup>
 import { ref } from "vue"
 import { useRouter } from "vue-router"
+import { Button } from "@/components/ui/button"
+import { FormField, FormItem, FormControl, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 useHead({
   title: "Wakati App | Mot de passe oublié"
@@ -31,16 +52,14 @@ const formData = reactive({
   email: '',
 })
 
-const submit = ({ valid }: any) => {
-  if (valid) {
-    console.log(JSON.parse(JSON.stringify(formData)))
-    setTimeout(() => {
-      router.push({
-        path: `/forgot-password/verify`,
-        query: { email: formData.email }
-      })
-    }, 1000)
-  }
+const submit = () => {
+  console.log(JSON.parse(JSON.stringify(formData)))
+   setTimeout(() => {
+    router.push({
+      path: `/forgot-password/verify`,
+      query: { email: formData.email }
+    })
+   }, 1000)
 }
 </script>
 
