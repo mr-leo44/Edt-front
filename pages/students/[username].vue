a4d3822f426930e2b457d64f3068ffcd0fadbdd5
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-03 09:05:28 +0100

    add student dashboard

---
+++ pages/students/[username].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3 class="text-2xl font-semibold">Bonjour {{ $route.params.username }}</h3>
+    <span class="text-sm">Ravi de vous revoir</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// import EdtCard from '@/components/student/EdtCard.vue';
+definePageMeta({
+  layout: 'student',
+})
+</script>
+
+<style></style>
