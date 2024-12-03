b0ab13e9ef2f5204e1a780723b2ee0cb1f32e716
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-03 09:05:43 +0100

    add student layout

---
+++ layouts/student.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavBar />
+  <div class="container py-20 px-6 md:px-12">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import NavBar from '~/components/student/NavBar.vue'
+
+</script>
+
+<style></style>
