9f08d6eba5886696658da867e9fdfa82a4723192
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-03 09:40:27 +0100

    update navbar

--- components/student/NavBar.vue
+++ components/student/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container flex bg-transparent dark:text-white justify-between items-center py-4 px-6 md:px-12 fixed z-30">
     <router-link :to="`/students/${$route.params.username}`">
-      <h3 class="text-lg md:text-2xl font-bold">
+      <h3 class="text-sm md:text-lg font-bold">
         Wakati <span class="text-emerald-700">App</span>
       </h3>
     </router-link>
-    <div class="flex justify-end gap-2 items-center text-sm md:text-md">
+    <div class="flex justify-end gap-2 items-center text-xs md:text-md">
       <router-link :to="`/students/${$route.params.username}`"
         class="dark:hover:text-neutral-700 dark:hover:bg-neutral-200 rounded p-2 dark:text-white duration-150 transition-colors">
         <span class="font-semibold">Aujourd'hui</span>
@@ -15,9 +15,6 @@
       </router-link>
       <router-link :to="`/students/${$route.params.username}`"
         class="dark:hover:text-neutral-700 dark:hover:bg-neutral-200 rounded p-2 dark:text-white duration-150 transition-colors">
-        <span class="font-semibold">Favoris</span>
-      </router-link>
-      <router-link to="/" class="dark:hover:text-neutral-700 dark:hover:bg-neutral-200 rounded p-2 dark:text-white duration-150 transition-colors">
         <span class="font-semibold">Favoris</span>
       </router-link>
     </div>
