1835fe7fb6945bcdc37d1203068183a3e421fff7
mr-leo44 <lionnelkaniki44@gmail.com>
2025-01-18 16:12:05 +0100

    update auth store

--- stores/auth.ts
+++ stores/auth.ts
@@ -7,44 +7,77 @@ export const useAuthStore = defineStore("auth", {
     currentUser: null as User | null,
   }),
 
+  getters: {
+    isAuthenticated: (state) => !!state.currentUser,
+  },
+
   actions: {
-    isAvailable(field: "email" | "phone" | "username", value: string): boolean {
+    isAvailable(field: "email" | "phone" | "username", value: string) {
       const dataStore = useDataStore();
-      return !dataStore.users.some((user) => user[field] === value);
-    },
-    registerUser(payload: {
-      user: {username: string, name: string, email: string, phone: string, password: string };
-      student: { university_id: number | null; promotion_id: number | null };
-    }): boolean {
-      const dataStore = useDataStore();
-      if (
-        !this.isAvailable("username", payload.user.username) ||
-        !this.isAvailable("email", payload.user.email) ||
-        !this.isAvailable("phone", payload.user.phone)
-      ) {
-        return false;
+      if(dataStore.users.some((user) => user[field] === value)) {
+        return { success: false, message: 'Cet ' + field + ' est déjà utilisé' }
       }
-      const userId = dataStore.users.length + 1;
-      const user: User = { ...payload.user, id: userId };
+    },
+    registerStudent({
+      name,
+      username,
+      email,
+      password,
+      password_confirmation,
+      phone,
+      university_id,
+      promotion_id,
+      type
+    } : {
+      name: string,
+      username: string,
+      email: string,
+      password: string,
+      password_confirmation: string,
+      phone: string,
+      university_id: number,
+      promotion_id: number,
+      type: "student" | "admin" | "admin_univ" | "admin_fac" | "professor"
+    }): { success: boolean, message: string } {
+      const dataStore = useDataStore();
+      if(password !== password_confirmation) {
+        return { success: false, message: 'Les mots de passe ne correspondent pas' }
+      }
+
+      this.isAvailable("username", username)
+      this.isAvailable("email", email)
+      this.isAvailable("phone", phone)
+
       const student: Student = {
         id: dataStore.students.length + 1,
-        promotion_id: payload.student.promotion_id,
-        university_id: payload.student.university_id,
-      };
+        university_id,
+        promotion_id,
+      }
+      dataStore.students.push(student)
+
+      const user: User = {
+        id: dataStore.users.length + 1,
+        name,
+        username,
+        email,
+        password,
+        phone,
+      }
+      dataStore.users.push(user)
+
       const account: Account = {
         id: dataStore.accounts.length + 1,
         user_id: user.id,
         accountable: {
-          type: "student",
-          id: student.id,
+          type,
+          id: student.id
         },
-        is_active: false,
-      };
-      dataStore.students.push(student);
-      dataStore.users.push(user);
-      dataStore.accounts.push(account);
+        is_active: true,
+      }
+      dataStore.accounts.push(account)
       this.currentUser = user;
-      return true;
+
+      return { success: true, message: 'inscription reussie' }
     },
     activateAccount(code: string): boolean {
       const dataStore = useDataStore();
