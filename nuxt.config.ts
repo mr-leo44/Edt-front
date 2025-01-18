0f3a6e7c9561ab23edc2cc82b225136ed5dd0f34
mr-leo44 <lionnelkaniki44@gmail.com>
2025-01-18 18:46:23 +0100

    add pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // compatibilityDate: "2024-04-03",
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
   modules: [
-    // "@pinia/nuxt",
+    "@pinia/nuxt",
     "@nuxt/icon",
     "@nuxtjs/tailwindcss",
     "shadcn-nuxt",
