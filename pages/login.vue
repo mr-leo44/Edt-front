be789c92d72af65427d86716471ebe0159c19147
mr-leo44 <lionnelkaniki44@gmail.com>
2025-01-21 12:22:35 +0100

    update login

--- pages/login.vue
+++ pages/login.vue
@@ -1,52 +1,91 @@
 <template>
   <div
-    class="flex justify-center items-end md:items-center h-screen bg-no-repeat bg-center md:bg-left-top bg-cover bg-[url('~/assets/images/login-bg-xs-sm.png')] md:bg-[url('~/assets/images/login-bg.jpg')] md:relative">
+    class="flex justify-center items-end md:items-center h-screen bg-no-repeat bg-center md:bg-left-top bg-cover bg-[url('~/assets/images/login-bg-xs-sm.png')] md:bg-[url('~/assets/images/login-bg.jpg')] md:relative"
+  >
     <div
-      class="md:absolute flex xl:right-16 bottom-0 md:bottom-auto md:right-8 items-center max-w-xl sm:max-w-3xl md:max-w-sm xl:max-w-lg w-full bg-white rounded-3xl mb-2 md:mb-0 px-8 py-6 text-gray-600">
+      class="md:absolute flex xl:right-16 bottom-0 md:bottom-auto md:right-8 items-center max-w-xl sm:max-w-3xl md:max-w-sm xl:max-w-lg w-full bg-white rounded-3xl mb-2 md:mb-0 px-8 py-6 text-gray-600"
+    >
       <div class="w-full">
         <h3 class="text-2xl xl:text-3xl text-center font-bold mb-8">
           Se connecter à <span class="text-emerald-700">Wakati App</span>
         </h3>
-        <form @submit.prevent="submit" class="flex flex-col space-y-3">
-          <FormField v-slot="{ componentField }" name="login">
+        <form @submit="onSubmit" class="flex flex-col space-y-3">
+          <FormField v-slot="{ componentField }" name="loginData">
             <FormItem>
-              <FormLabel class="text-sm text-neutral-700 font-semibold">Username ou Email</FormLabel>
+              <FormLabel class="text-sm text-neutral-700 font-semibold"
+                >Username ou Email</FormLabel
+              >
               <FormControl>
-                <Input type="text"
+                <Input
+                  type="text"
                   class="w-full p-2 sm:px-4 text-sm sm:text-md rounded placeholder-neutral-800 border border-neutral-600 focus:border-neutral-700 outline-none"
-                  v-model="formData.login" autocomplete="off" v-bind="componentField" />
+                  autocomplete="off"
+                  v-bind="componentField"
+                />
               </FormControl>
+              <FormMessage class="text-xs text-red-500" />
             </FormItem>
           </FormField>
           <FormField v-slot="{ componentField }" name="password">
             <FormItem>
-              <FormLabel class="text-sm text-neutral-700 font-semibold">Mot de passe</FormLabel>
+              <FormLabel class="text-sm text-neutral-700 font-semibold"
+                >Mot de passe</FormLabel
+              >
               <FormControl>
-                <Input type="password"
+                <Input
+                  type="password"
                   class="w-full p-2 sm:px-4 text-sm sm:text-md rounded placeholder-neutral-800 border border-neutral-600 focus:border-neutral-700 outline-none"
-                  v-model="formData.password" autocomplete="off" v-bind="componentField" />
+                  autocomplete="off"
+                  v-bind="componentField"
+                />
               </FormControl>
+              <FormMessage class="text-xs text-red-500" />
             </FormItem>
           </FormField>
           <div class="flex justify-between items-center mt-3 mb-8">
-            <div class="flex items-center space-x-2 font-medium text-xs sm:text-sm">
-              <Checkbox id="remember_me" class="border-neutral-700" v-model="formData.remember_me" />
-              <Label for="remember_me">Se souvenir de moi</Label>
+            <div
+              class="flex items-center space-x-2 font-medium text-xs sm:text-sm"
+            >
+              <FormField
+                v-slot="{ value, handleChange }"
+                type="checkbox"
+                name="remember_me"
+              >
+                <FormItem>
+                  <FormControl>
+                    <div class="flex items-center gap-1">
+                      <Checkbox
+                        :checked="value"
+                        @update:checked="handleChange"
+                        class="border-neutral-700"
+                      />
+                      <FormLabel>Se souvenir de moi</FormLabel>
+                    </div>
+                  </FormControl>
+                  <FormMessage class="text-xs text-red-500" />
+                </FormItem>
+              </FormField>
             </div>
-            <NuxtLink to="/forgot-password" class="font-medium text-xs sm:text-sm">
+            <NuxtLink
+              to="/forgot-password"
+              class="font-medium text-xs sm:text-sm"
+            >
               Mot de passe oublié?
             </NuxtLink>
           </div>
 
-          <Button type="submit"
-            class="p-3 bg-emerald-500 rounded text-white hover:bg-emerald-700 hover:text-white font-semibold text-sm 2xl:text-md uppercase border-none">
+          <Button
+            type="submit"
+            class="p-3 bg-emerald-500 rounded text-white hover:bg-emerald-700 hover:text-white font-semibold text-sm 2xl:text-md uppercase border-none"
+          >
             Se connecter
           </Button>
         </form>
 
         <p class="my-4 text-xs text-center">
-          En cliquant sur le bouton se connecter ci-dessous, vous acceptez <span class="text-emerald-700">les
-            conditions de service</span> de wakati app
+          En cliquant sur le bouton se connecter ci-dessous, vous acceptez
+          <span class="text-emerald-700">les conditions de service</span> de
+          wakati app
         </p>
         <div class="text-center mt-8 mb-4 font-bold text-xs sm:text-sm">
           <span>Pas de compte? </span>
@@ -54,44 +93,64 @@
             S'inscrire
           </NuxtLink>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useRouter } from 'vue-router'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Checkbox } from '@/components/ui/checkbox'
+import { useRouter } from "vue-router";
+import { Button } from "@/components/ui/button";
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as z from "zod";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-} from '@/components/ui/form'
+} from "@/components/ui/form";
+
+import { useAuthStore } from "~/stores/auth";
 
 useHead({
-  title: "Wakati App | login"
-})
+  title: "Wakati App | login",
+});
 
-const router = useRouter()
-const formData = reactive({
-  login: '',
-  password: '',
-  remember_me: false,
-})
+const authStore = useAuthStore()
+const router = useRouter();
 
-const submit = () => {
-  try {
-    console.log(JSON.parse(JSON.stringify(formData)))
-    router.push(`/students/${formData.login}`)
-  } catch (error) {
-    console.error('Erreur de la connexion :', error);
+const formSchema = toTypedSchema(
+  z.object({
+    loginData: z.string(),
+    password: z.string(),
+    remember_me: z.boolean().default(false),
+  })
+);
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+});
+
+const onSubmit = handleSubmit((values) => {
+  console.log(values);
+  
+  const result = authStore.loginUser({
+    login: values.loginData,
+    password: values.password,
+    remember_me: values.remember_me,
+  })
+
+  if(result.success) {
+    console.log(result.user)
+    router.push(`students/${result.user?.username}`)
+  } else {
+    console.error(result.message)
   }
-}
+});
 </script>
 
 <style></style>
